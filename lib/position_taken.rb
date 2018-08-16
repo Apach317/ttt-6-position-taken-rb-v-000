1028113259583f267966610ba70389f36e51d59a
Ron Lynch <lynch.ronald1@gmail.com>
2018-08-16 23:05:54 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,12 +1,12 @@
 # code your #position_taken? method here!
 
-def position_taken?(board, index).to be(true)
+def position_taken?(board, index).tobe(true)
   if board[index] = ("x" || "o")
     true
   end
 end
 
-def position_taken?(board, index).to be(false)
+def position_taken?(board, index).tobe(false)
   if board[index] = (" " || nil)
     false
   end

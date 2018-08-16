707dcc6e3d069c8a35602055ace60118340b98e9
Ron Lynch <lynch.ronald1@gmail.com>
2018-08-16 21:33:33 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,11 @@
 # code your #position_taken? method here!
 
 def position_taken? (board, index)
-  if value = X
-    false
-  elsif value = O
-    false
-  elsif value =  " "
+  if "X"
+    true
+  elsif "O"
+    true
+  elsif " "
     false
   else
     false
